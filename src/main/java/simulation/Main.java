b669495c96a0a9378520237d351e970bf8b8a4f5
aveasura <aveasura@gmail.com>
2026-02-15 07:49:13 +0300

    ref: split Simulation

--- src/main/java/simulation/Main.java
+++ src/main/java/simulation/Main.java
@@ -13,22 +13,25 @@ import simulation.path.PathFinder;
 import simulation.path.rules.DefaultMoveRulesProvider;
 import simulation.path.rules.MoveRulesProvider;
 import simulation.renderer.ConsoleRenderer;
+import simulation.runner.SimulationRunner;
 
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         SimulationMap map = new SimulationMap(10, 10); // пока что затычка
+        int entityQuantity = 15;
+
         Factory<Entity> factory = new SimulationFactory();
         NeighborsFinder neighborsFinder = new NeighborsFinder();
         PathFinder pathFinder = new BreadthFirstSearch(neighborsFinder);
         MoveRulesProvider provider = new DefaultMoveRulesProvider();
-        int entityQuantity = 15;
         List<Action> init = List.of(new InitialSpawnEntitiesAction(factory, entityQuantity));
         List<Action> turn = List.of(new MoveCreaturesAction(pathFinder, provider, neighborsFinder));
         ConsoleRenderer renderer = new ConsoleRenderer();
         Simulation game = new Simulation(map, init, turn, renderer);
+        SimulationRunner app = new SimulationRunner(game);
 
-        game.startSimulation();
+        app.run(10, 500);
     }
 }
--- src/main/java/simulation/Simulation.java
+++ src/main/java/simulation/Simulation.java
@@ -6,10 +6,10 @@ import simulation.renderer.Renderer;
 
 import java.util.List;
 
-
 public class Simulation {
 
-    private int stepCounter = 0;
+    private int step = 0;
+
     private final SimulationMap map;
     private final List<Action> init;
     private final List<Action> turn;
@@ -22,32 +22,18 @@ public class Simulation {
         this.renderer = renderer;
     }
 
-    public void startSimulation() {
-        for (Action a : init) {
-            a.execute(map);
-        }
-        renderer.renderMap(map, stepCounter);
-
-        // тут будет цикл
-        while (true) {
-            nextTurn();
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
+    public void initSimulation() {
+        init.forEach(a -> a.execute(map));
+        renderer.renderMap(map, step);
     }
 
-    private void nextTurn() {
-        for (Action t : turn) {
-            t.execute(map);
-        }
-        stepCounter++;
-        renderer.renderMap(map, stepCounter);
+    public void step() {
+        turn.forEach(a -> a.execute(map));
+        step++;
+        renderer.renderMap(map, step);
     }
 
-    private void pauseSimulation() {
-
+    public int getStep() {
+        return step;
     }
 }
