2063444d5704ee50aecbe055cb7b7225718da178
aveasura <aveasura@gmail.com>
2026-03-14 22:13:12 +0300

    refactor: extract creature collection logic

--- src/main/java/org/simulation/action/MoveEntityAction.java
+++ src/main/java/org/simulation/action/MoveEntityAction.java
@@ -3,10 +3,11 @@ package org.simulation.action;
 import org.simulation.entity.Entity;
 import org.simulation.entity.creature.Creature;
 import org.simulation.game.GameMap;
-import org.simulation.game.Position;
 import org.simulation.path.PathFinder;
 
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
 
 public class MoveEntityAction implements Action {
@@ -19,18 +20,28 @@ public class MoveEntityAction implements Action {
 
     @Override
     public void execute(GameMap gameMap) {
-        Map<Position, Entity> entities = gameMap.toMap();
-
-        for (Entity entity : entities.values()) {
-            if (!(entity instanceof Creature creature)) {
-                continue;
-            }
-
+        List<Creature> creatures = getCreaturesSnapshot(gameMap);
+        for (Creature creature : creatures) {
             if (!gameMap.containsEntity(creature)) {
                 continue;
             }
 
             creature.makeMove(gameMap, pathFinder);
         }
+    }
+
+    private List<Creature> getCreaturesSnapshot(GameMap gameMap) {
+        List<Creature> creatures = new ArrayList<>();
+        Collection<Entity> entities = gameMap.toMap().values();
+
+        for (Entity entity : entities) {
+            if (!(entity instanceof Creature creature)) {
+                continue;
+            }
+
+            creatures.add(creature);
+        }
+
+        return creatures;
     }
 }
